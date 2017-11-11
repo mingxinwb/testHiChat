574272d54e4280f307f177507ba2fb5035584aa5
mingxinwb <mingxinwb@gmail.com>
2017-11-10 23:23:27 -0800

    set nickname

--- HiChat/Hichat-node+socket/server.js
+++ HiChat/Hichat-node+socket/server.js
@@ -4,6 +4,8 @@ const http = require('http');
 const server = http.createServer(app);
 const io = require('socket.io')(server);
 
+var users = [];
+
 app.use('/', express.static(__dirname + '/www/'));
 
 // listening on port:8080
@@ -12,13 +14,31 @@ server.listen(8080, () => {
 });
 
 io.on('connection', (socket) => {
-    socket.on('foo', (data) => {
-        console.log(data);
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
     });
+
+    socket.on('login', (nickname) => {
+        if (users.indexOf(nickname) > -1) {
+            socket.emit('nickExisted');
+        } else {
+            console.log(nickname + ' connected');
+            socket.userIndex = users.length;
+            socket.nickname = nickname;
+            users.push(nickname);
+            socket.emit('loginSuccess');
+            io.sockets.emit('system', nickname);
+        };
+    })
 });
 
 
-// for test about socket.io
+
+
+
+// // for test about socket.io
 // io.on('connection', (socket) => {
 //     console.log('a user connected');
 
@@ -32,6 +52,10 @@ io.on('connection', (socket) => {
 //     });
 
 //     socket.on('pushBtn', (data) => {
+//         console.log(data);
+//     });
+
+//     socket.on('foo', (data) => {
 //         console.log(data);
 //     });
 // });
--- HiChat/Hichat-node+socket/www/scripts/hichat.js
+++ HiChat/Hichat-node+socket/www/scripts/hichat.js
@@ -3,18 +3,44 @@ window.onload = () => {
     chat.init();
 };
 
-const Chat = function() {
+var Chat = function() {
     this.socket = null;
 };
 
 Chat.prototype = {
     init: () => {
+        const that = this;
+
         this.socket = io.connect();
         this.socket.on('connect', () => {
-            console.log('connected to server.');
+            console.log('client connected to server now.');
             document.getElementById('info').textContent = 'get yourself a nickname :)';
             document.getElementById('nickWrapper').style.display = 'block';
             document.getElementById('nicknameInput').focus();
         });
+
+        // set nickname
+        document.getElementById('loginBtn').addEventListener('click', () => {
+            const nickname = document.getElementById('nicknameInput').value;
+            if (nickname.trim().length !=0) {
+                that.socket.emit('login', nickname);
+            } else {
+                document.getElementById('nicknameInput').focus();
+            };
+        }, false);
+
+        // nickname existed
+        this.socket.on('nickExisted', () => {
+            document.getElementById('info').textContent = 'oops...nickname is taken, choose another pls';
+        });
+
+        // log in successed, remove loginWrapper, can chat now
+        this.socket.on('loginSuccess', () => {
+            const nickname = document.getElementById('nicknameInput').value
+            console.log(nickname + ' has login.');
+            document.title = 'HiChat | ' + nickname;
+            document.getElementById('loginWrapper').style.display = 'none';
+            document.getElementById('messageInput').focus();
+        })
     }
 };
